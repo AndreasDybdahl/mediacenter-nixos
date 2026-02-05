1b32ac07022206531837797efbb7b965b3a555e9
Andreas Dybdahl <andreas.dyb@gmail.com>
2026-02-05 21:48:09 +0100

    Trying to add japanese characters

--- users/andreas/default.nix
+++ users/andreas/default.nix
@@ -15,5 +15,17 @@
     vlc
   ];
 
+  # Chinese, Korean, and Japanese fonts
+  config.fonts.packages = [
+    pkgs.cascadia-code
+    pkgs.fira-code
+    pkgs.fira-code-nerdfont
+    pkgs.fira-code-symbols
+
+    # Chinese, Korean, and Japanese fonts
+    pkgs.noto-fonts-cjk-sans
+    pkgs.noto-fonts-cjk-serif
+  ];
+
   home = ./home.nix;
 }
